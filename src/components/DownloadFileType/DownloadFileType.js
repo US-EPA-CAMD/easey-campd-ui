c9f70c10c4157d4c84bb5207f6d688d546bc3dd5
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-05-17 14:16:34 -0400

    added select download file type

--- src/components/DownloadFileType/DownloadFileType.js
+++ src/components/DownloadFileType/DownloadFileType.js
@@ -7,22 +7,23 @@ const DownloadFileType = (props) => {
       className="height-10 border-2px radius-md bg-primary-lighter"
       style={{ borderColor: '#005EA2' }}
     >
-      <div className="display-flex flex-row flex-justify flex-align-self-center padding-y-2 padding-x-3 font-sans-sm">
+      <div className="display-flex flex-row flex-justify flex-align-self-center font-sans-sm">
         <Radio
-          className="padding-right-2"
+          className="padding-right-1"
           id="csv"
           name="'input-radio'"
           label="CSV"
+          defaultChecked
         />
         <Radio
-          className="padding-left-1 padding-right-3"
+          className="padding-right-3"
           id="json"
           name="'input-radio'"
           label="JSON"
         />
         <Button
           type="button"
-          className="margin-right-0"
+          className="margin-right-3"
           onClick={props.onClick}
         >
           Download
