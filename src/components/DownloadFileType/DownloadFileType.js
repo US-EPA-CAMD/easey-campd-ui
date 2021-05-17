8614500b875543af669eed806873b20f3e42b186
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-05-17 15:03:18 -0400

    adjusted radio label font weight to be bolder

--- src/components/DownloadFileType/DownloadFileType.js
+++ src/components/DownloadFileType/DownloadFileType.js
@@ -4,21 +4,21 @@ import { Button, Radio } from '@trussworks/react-uswds';
 const DownloadFileType = (props) => {
   return (
     <div
-      className="height-10 border-2px radius-md bg-primary-lighter"
+      className="height-10 border-2px radius-lg bg-primary-lighter"
       style={{ borderColor: '#005EA2' }}
     >
       <div className="display-flex flex-row flex-justify flex-align-self-center font-sans-sm">
         <Radio
-          className="padding-right-1"
           id="csv"
-          name="'input-radio'"
+          name="input-radio"
           label="CSV"
+          color="bg-primary"
           defaultChecked
         />
         <Radio
           className="padding-right-3"
           id="json"
-          name="'input-radio'"
+          name="input-radio"
           label="JSON"
         />
         <Button
