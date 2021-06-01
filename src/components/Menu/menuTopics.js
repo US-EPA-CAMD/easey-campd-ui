2d9054ef359bb653f70960f001ecf413ae257347
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-06-01 14:02:02 -0400

    fixed code smell

--- src/components/Menu/menuTopics.js
+++ src/components/Menu/menuTopics.js
@@ -1,14 +1,14 @@
 export const collapsableFooterTopics = [
-    {
-        link: "https://www.epa.gov/accessibility",
-        name: 'Accessibility'
-    },
-    {
-        link: "https://www.epa.gov/privacy",
-        name: 'Privacy'
-    },
-    {
-        link: "https://www.epa.gov/privacy/privacy-and-security-notice",
-        name: 'Privacy and Security Notice'
-    },
-]
+  {
+    link: 'https://www.epa.gov/accessibility',
+    name: 'Accessibility',
+  },
+  {
+    link: 'https://www.epa.gov/privacy',
+    name: 'Privacy',
+  },
+  {
+    link: 'https://www.epa.gov/privacy/privacy-and-security-notice',
+    name: 'Privacy and Security Notice',
+  },
+];
