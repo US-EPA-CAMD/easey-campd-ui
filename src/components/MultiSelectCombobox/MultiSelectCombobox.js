3f79bc62a6101759bfea96f4bb871952d97e793a
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-06-21 16:34:47 -0400

    sonar cloud vulnerability

--- src/components/MultiSelectCombobox/MultiSelectCombobox.js
+++ src/components/MultiSelectCombobox/MultiSelectCombobox.js
@@ -151,6 +151,10 @@ const MultiSelectCombobox = ({
         }
       </div>
       {window.addEventListener('click', function(e){
+        // cross origin check compliant
+        if (e.origin !== undefined && !e.origin.endsWith('epa.gov'))
+          return;
+
         const multiSelectComboboxDiv = document.getElementById('multi-select-combobox');
         if (multiSelectComboboxDiv && !multiSelectComboboxDiv.contains(e.target)){
           setShowListBox(false);
