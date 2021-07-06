976a365e32296180023c168d53b3457600db2d67
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-07-06 12:02:04 -0400

    updated unit test coverage for data preview elements

--- src/components/dataPreview/ManageDataPreview/ManageDataPreview.test.js
+++ src/components/dataPreview/ManageDataPreview/ManageDataPreview.test.js
@@ -1,25 +1,105 @@
-import React from "react";
-import ManageDataPreview from "./ManageDataPreview";
-import { render, fireEvent } from "@testing-library/react";
-import configureStore from "../../../store/configureStore.dev";
-import { Provider } from "react-redux";
-import initialState from "../../../store/reducers/initialState";
+import React from 'react';
+import ManageDataPreview from './ManageDataPreview';
+import { render, fireEvent } from '@testing-library/react';
+import configureStore from '../../../store/configureStore.dev';
+import { Provider } from 'react-redux';
+import initialState from '../../../store/reducers/initialState';
 
-initialState.customDataDownload.dataType= "EMISSIONS";
-initialState.customDataDownload.dataSubType= "Hourly Emissions";
+initialState.customDataDownload.dataType = 'EMISSIONS';
+initialState.customDataDownload.dataSubType = 'Hourly Emissions';
 initialState.customDataDownload.appliedFilters = [
-  {key:"Time Period", values:["1/1/2019 - 1/1/2019"]}
+  { key: 'Time Period', values: ['1/1/2019 - 1/1/2019'] },
 ];
 initialState.filterCriteria.timePeriod = {
-  startDate: "2019-01-01", endDate: "2019-01-01", opHrsOnly:false
+  startDate: '2019-01-01',
+  endDate: '2019-01-01',
+  opHrsOnly: false,
 };
-const store = configureStore(initialState);
+let store = configureStore(initialState);
 
+describe('ManageDataPreview', () => {
+  test('Check that the  component properly renders', () => {
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <ManageDataPreview dataType="EMISSIONS" />
+      </Provider>
+    );
+    const previewButton = getByRole('button', { name: 'Preview Data' });
+    expect(previewButton).toBeDefined();
+    fireEvent.click(previewButton);
+    const dataPreviewHeader = getByText('Data Preview');
+    expect(dataPreviewHeader).toBeDefined();
+  });
 
-describe("ManageDataPreview", () => {
-  test("Check that the  component properly renders", () => {
-    const { getByRole, getByText } = render(<Provider store={store}><ManageDataPreview dataType="EMISSIONS" /></Provider>);
-    const previewButton = getByRole('button' , { name: "Preview Data" });
+  test('Daily Emissions', () => {
+    initialState.customDataDownload.dataSubType = 'Daily Emissions';
+    store = configureStore(initialState);
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <ManageDataPreview dataType="EMISSIONS" />
+      </Provider>
+    );
+    const previewButton = getByRole('button', { name: 'Preview Data' });
+    expect(previewButton).toBeDefined();
+    fireEvent.click(previewButton);
+    const dataPreviewHeader = getByText('Data Preview');
+    expect(dataPreviewHeader).toBeDefined();
+  });
+
+  test('Monthly Emissions', () => {
+    initialState.customDataDownload.dataSubType = 'Monthly Emissions';
+    store = configureStore(initialState);
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <ManageDataPreview dataType="EMISSIONS" />
+      </Provider>
+    );
+    const previewButton = getByRole('button', { name: 'Preview Data' });
+    expect(previewButton).toBeDefined();
+    fireEvent.click(previewButton);
+    const dataPreviewHeader = getByText('Data Preview');
+    expect(dataPreviewHeader).toBeDefined();
+  });
+
+  test('Quarterly Emissions', () => {
+    initialState.customDataDownload.dataSubType = 'Quarterly Emissions';
+    store = configureStore(initialState);
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <ManageDataPreview dataType="EMISSIONS" />
+      </Provider>
+    );
+    const previewButton = getByRole('button', { name: 'Preview Data' });
+    expect(previewButton).toBeDefined();
+    fireEvent.click(previewButton);
+    const dataPreviewHeader = getByText('Data Preview');
+    expect(dataPreviewHeader).toBeDefined();
+  });
+
+  test('Annual Emissions', () => {
+    initialState.customDataDownload.dataSubType = 'Annual Emissions';
+    store = configureStore(initialState);
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <ManageDataPreview dataType="EMISSIONS" />
+      </Provider>
+    );
+    const previewButton = getByRole('button', { name: 'Preview Data' });
+    expect(previewButton).toBeDefined();
+    fireEvent.click(previewButton);
+    const dataPreviewHeader = getByText('Data Preview');
+    expect(dataPreviewHeader).toBeDefined();
+  });
+
+  test('Ozone Emissions', () => {
+    initialState.customDataDownload.dataSubType = 'Ozone Season Emissions';
+    store = configureStore(initialState);
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <ManageDataPreview dataType="EMISSIONS" />
+      </Provider>
+    );
+    const previewButton = getByRole('button', { name: 'Preview Data' });
     expect(previewButton).toBeDefined();
     fireEvent.click(previewButton);
     const dataPreviewHeader = getByText('Data Preview');
--- src/components/filterCriteria/timePeriod/TimePeriod/TimePeriod.test.js
+++ src/components/filterCriteria/timePeriod/TimePeriod/TimePeriod.test.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, getByRole } from '@testing-library/react';
 import { TimePeriod } from './TimePeriod';
-
-const initTimePeriod = {
-    startDate: null,
-    endDate: null,
-    opHrsOnly: true
-}
-
-const timePeriod = {
-  startDate: "2019-01-01",
-  endDate: "2019-01-01",
-  opHrsOnly: true
-}
+import userEvent from '@testing-library/user-event';
+import TimePeriodRender from './TimePeriodRender';
 
 describe('Emissions TimePeriod Component', () => {
+
+  /* -- TimePeriodFullDates -- */
   it('renders form elements without errors', () => {
+    const initTimePeriod = {
+      startDate: null,
+      endDate: null,
+      opHrsOnly: true
+  }
     const { getByTestId, getAllByTestId, getByRole } = render(
       <TimePeriod
         timePeriod={initTimePeriod}
@@ -43,6 +40,11 @@ describe('Emissions TimePeriod Component', () => {
     const dispather = (formState) =>{
       updatedTimePeriod = formState;
     };
+    const timePeriod = {
+      startDate: "2019-01-01",
+      endDate: "2019-01-01",
+      opHrsOnly: true
+    }
     const { getByText, getByRole } = render(
       <TimePeriod
         timePeriod={timePeriod}
@@ -60,4 +62,99 @@ describe('Emissions TimePeriod Component', () => {
     expect(updatedTimePeriod.startDate).toBe(timePeriod.startDate)
     expect(updatedTimePeriod.opHrsOnly).toBe(false)
   });
+
+    /* -- TimePeriodYear -- */
+ 
+    it('handles YEAR interactions appropriately', () => {
+      let updatedTimePeriod;
+      const dispather = (formState) => {
+        updatedTimePeriod = formState;
+      };
+      const timePeriod = {
+        year: { yearString: '2019,2020', yearArray: [2019, 2020] },
+        month: [],
+        quarter: [],
+      };
+      const { getByText, getByTestId } = render(
+        <TimePeriod
+          timePeriod={timePeriod}
+          updateTimePeriodDispatcher={dispather}
+          addAppliedFilterDispatcher={jest.fn()}
+          appliedFilters={['timePeriod']}
+          closeFlyOutHandler={jest.fn()}
+          showYear={true}
+          isAnnual={true}
+        />
+      );
+      const textBox = getByTestId('textInput');
+      userEvent.type(textBox, '2019,2020');
+      const applyFilterButton = getByText('Apply Filter').closest('button');
+      expect(applyFilterButton).not.toBeDisabled();
+      fireEvent.click(applyFilterButton);
+      expect(updatedTimePeriod.year.yearString).toBe(timePeriod.year.yearString);
+    });
+
+    it('handles MONTH interactions appropriately', () => {
+      let updatedTimePeriod;
+      const dispather = (formState) => {
+        updatedTimePeriod = formState;
+      };
+      const timePeriod = {
+        year: { yearString: '2019,2020', yearArray: [2019, 2020] },
+        month: [{id: 1, label: 'January', selected: false}],
+        quarter: [],
+      };
+      const { getByText, getByTestId, getByLabelText } = render(
+        <TimePeriod
+          timePeriod={timePeriod}
+          updateTimePeriodDispatcher={dispather}
+          addAppliedFilterDispatcher={jest.fn()}
+          appliedFilters={['timePeriod']}
+          closeFlyOutHandler={jest.fn()}
+          showYear={true}
+          showMonth={true}
+        />
+      );
+      const textBox = getByTestId('textInput');
+      userEvent.type(textBox, '2019,2020');
+      const checkbox = getByLabelText('January')
+      fireEvent.click(checkbox)
+      const applyFilterButton = getByText('Apply Filter').closest('button');
+      expect(applyFilterButton).not.toBeDisabled();
+      fireEvent.click(applyFilterButton);
+      expect(updatedTimePeriod.year.yearString).toBe(timePeriod.year.yearString);
+      expect(updatedTimePeriod.month[0].selected).toBe(true);
+    });
+
+    it('handles QUARTER interactions appropriately', () => {
+      let updatedTimePeriod;
+      const dispather = (formState) => {
+        updatedTimePeriod = formState;
+      };
+      const timePeriod = {
+        year: { yearString: '2019,2020', yearArray: [2019, 2020] },
+        month: [],
+        quarter: [{id: 1, label: 'Q1', selected: false}],
+      };
+      const { getByText, getByTestId, getByLabelText } = render(
+        <TimePeriod
+          timePeriod={timePeriod}
+          updateTimePeriodDispatcher={dispather}
+          addAppliedFilterDispatcher={jest.fn()}
+          appliedFilters={['timePeriod']}
+          closeFlyOutHandler={jest.fn()}
+          showYear={true}
+          showQuarter={true}
+        />
+      );
+      const textBox = getByTestId('textInput');
+      userEvent.type(textBox, '2019,2020');
+      const checkbox = getByLabelText('Q1')
+      fireEvent.click(checkbox)
+      const applyFilterButton = getByText('Apply Filter').closest('button');
+      expect(applyFilterButton).not.toBeDisabled();
+      fireEvent.click(applyFilterButton);
+      expect(updatedTimePeriod.year.yearString).toBe(timePeriod.year.yearString);
+      expect(updatedTimePeriod.quarter[0].selected).toBe(true);
+    });
 });
