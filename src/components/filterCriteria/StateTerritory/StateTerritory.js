a8a4b331a07da8231e9dda1af9ba4289c5d7738e
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-12-14 10:14:17 -0700

    enabled filter logic for compliance states

--- src/components/filterCriteria/StateTerritory/StateTerritory.js
+++ src/components/filterCriteria/StateTerritory/StateTerritory.js
@@ -35,7 +35,7 @@ const StateTerritory = ({
 
   useEffect(()=>{
     if(applyFilterClicked){
-      if(dataType === "EMISSIONS" || dataSubType === "Holdings" || dataSubType === "Account Information"){
+      if(dataType === "EMISSIONS" || dataType === "COMPLIANCE" || dataSubType === "Holdings" || dataSubType === "Account Information"){
         if(filterCriteria.filterMapping.length>0){
           engageFilterLogic(dataType, dataSubType, filterToApply, JSON.parse(JSON.stringify(filterCriteria)), updateFilterCriteriaDispacher);
         }
