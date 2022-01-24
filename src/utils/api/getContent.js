b4b0243fdc5a3f26b685abfec38400f2a33638d4
Abu Fofanah <abufofanah@cvpcorp.com>
2022-01-24 09:18:00 -0500

    clean up

--- src/utils/api/getContent.js
+++ src/utils/api/getContent.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import config from '../../config';
 import { handleError, handleResponse } from './apiUtils';
 const getContent = async (path) => {
-  let url = `${config.services.content.uri}${path}`;
+  const url = `${config.services.content.uri}${path}`;
 
   return axios
     .get(url)
