e12aedfcb887f7d69912652902c0fa3ea527f164
Courtney Myers <courtney.myers@erg.com>
2022-03-02 17:43:26 -0500

    Add initial ToolsGalleryPage component test

---
+++ src/components/ToolsGalleryPage/ToolsGalleryPage.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import ToolsGalleryPage from './ToolsGalleryPage';
+
+describe('Tools Gallery Page Component', () => {
+  test('should render content without error', () => {
+    const { getByText } = render(<MemoryRouter><ToolsGalleryPage/></MemoryRouter>);
+    expect(getByText('Tools Gallery')).toBeDefined();
+  });
+});
