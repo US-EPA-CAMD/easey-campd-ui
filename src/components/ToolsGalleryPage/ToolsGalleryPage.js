a4411ad6c2a8d8a4581d6590a0d48074d8d1e90d
Courtney Myers <courtney.myers@erg.com>
2022-03-03 17:54:11 -0500

    Add initial ToolCard component, ToolsGalleryPage styles, and mock tool data

--- src/components/ToolsGalleryPage/ToolsGalleryPage.js
+++ src/components/ToolsGalleryPage/ToolsGalleryPage.js
@@ -1,35 +1,93 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
+import { Preloader } from "@us-epa-camd/easey-design-system";
 
-import { metaAdder } from '../../utils/document/metaAdder';
+import { placeholderTools } from "./mockData"; // TEMPORARY
+import "./ToolsGalleryPage.scss";
+import { metaAdder } from "../../utils/document/metaAdder";
+
+const ToolCard = ({ data }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="grid-col-12 tablet:grid-col-6 desktop:grid-col-4">
+      <div className="margin-1">
+        <div
+          className="campd-tool padding-2 radius-md shadow-1 hover:shadow-3"
+          data-expanded={expanded}
+        >
+          <p className="campd-tool-name margin-0 font-sans-sm line-height-sans-2 text-bold">
+            <a
+              className="usa-link underline-base-lightest hover:underline-accent-cool"
+              href={data.url}
+            >
+              {data.name}
+            </a>
+          </p>
+
+          <p className="campd-tool-summary font-sans-2xs line-height-sans-4">
+            {data.summary}
+          </p>
+
+          <div className="campd-tool-meta font-sans-3xs line-height-sans-3 text-base">
+            {data.meta}
+          </div>
+
+          <div className="campd-tool-footer">
+            <button
+              className="usa-button"
+              onClick={(ev) => setExpanded(!expanded)}
+            >
+              {expanded ? "Show less" : "Show more"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const ToolsGalleryPage = () => {
   useEffect(() => {
-    document.title = 'Tools Gallery | CAMPD | US EPA';
+    document.title = "Tools Gallery | CAMPD | US EPA";
+    metaAdder("description", "(TODO)");
+    metaAdder("keywords", "(TODO)");
   }, []);
 
-  metaAdder(
-    'description',
-    '(TODO)'
-  );
+  const [loading, setLoading] = useState(false);
+  const [tools, setTools] = useState([]);
 
-  metaAdder(
-    'keywords',
-    '(TODO)'
-  );
+  useEffect(() => {
+    setLoading(true);
+
+    // NOTE: web service fetch simulated...
+    // these placeholder tools would be likely fetched from a web service
+    setTimeout(() => {
+      setLoading(false);
+      setTools(placeholderTools);
+    }, 1000);
+  }, []);
 
   return (
     <div className="grid-container-widescreen">
       <h1 className="font-sans-2xl text-bold">Tools Gallery</h1>
+
       <p className="font-sans-lg line-height-sans-6">
         CAMPD’s Tools Gallery showcases compliance-related dashboards and maps
         created by users across EPA, making datasets easier to explore and
-        encouraging collaboration.
-        &nbsp;
-        <mark><small>(placeholder text)</small></mark>
+        encouraging collaboration <mark>(placeholder text)</mark>.
       </p>
+
       <p className="font-sans-sm line-height-sans-5">
-        (Placeholder for tools)
+        <mark>(tools intro text?)</mark>
       </p>
+
+      <div className="grid-row">
+        {loading ? (
+          <Preloader />
+        ) : (
+          tools.map((tool) => <ToolCard key={tool.id} data={tool} />)
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/components/ToolsGalleryPage/mockData.js
@@ -0,0 +1,336 @@
+import React from "react";
+
+const placeholderTools = [
+  {
+    id: 1,
+    name: "Underground Storage Tank Finder",
+    img: "",
+    url: "#",
+    summary: (
+      <>
+        <p>
+          This application contains information about, and locations of, leaking
+          underground storage tanks, underground storage tank facilities, and
+          underground storage tanks. Data is current as of 2018-2021. Data was
+          collected from state sources and standardized into a national profile
+          by EPA's Office of Underground Storage Tanks, Office of Research and
+          Development, and the Association of State and Territorial Solid Waste
+          Management Officials.
+        </p>
+      </>
+    ),
+    meta: (
+      <>
+        <p>
+          <strong>Source Data Description:</strong> State Sourced. For more
+          information see UST Finder User Manual:{" "}
+          <a className="usa-link" href="https://tinyurl.com/p5txdpaa">
+            https://tinyurl.com/p5txdpaa
+          </a>
+        </p>
+        <p>
+          <strong>Data Refresh Schedule:</strong>
+          NA
+        </p>
+        <p>
+          <strong>Intended Audience or Use Cases:</strong> General Public
+        </p>
+        <p>
+          <strong>Caveats and Limitations:</strong> U.S. EPA has posted
+          information through this application as a convenience to the
+          application’s users. Although U.S. EPA has made every effort to ensure
+          the accuracy of the information posted through this application, users
+          of this application should not rely on information relating to
+          environmental laws and regulations posted on this application.
+          Application users are solely responsible for ensuring that they are in
+          compliance with all relevant environmental laws and regulations. More
+          specifically, with respect to the application and its documentation
+          and any data or other information obtained through the application,
+          neither the U.S. Government nor any of its employees, makes any
+          warranty, express or implied, including the warranties of
+          merchantability and fitness for a particular purpose, or assumes any
+          legal liability or responsibility for the accuracy, completeness, or
+          usefulness of any information, data apparatus, product, or process
+          disclosed, or represents that its use would not infringe privately
+          owned rights. The data is collected from various sources, including
+          those from outside the federal government, and may be modified over
+          time without notice to improve spatial and attribute accuracy. U.S.
+          EPA disclaims responsibility for the spatial accuracy and attribution
+          of GIS features and makes no warranty concerning same. Execution of
+          the application, user downloading of data, and any modification to the
+          user’s system configuration files must be conducted at the user's own
+          risk. This application may not be completely free of errors and may
+          not be applicable for all purposes. In no event will the U.S. EPA be
+          liable for direct, indirect, special, incidental, or consequential
+          damages arising out of the use of the application, user-downloaded
+          data, and/or associated documentation.
+        </p>
+        <p>
+          <strong>Help Documentation:</strong>{" "}
+          <a className="usa-link" href="https://tinyurl.com/p5txdpaa">
+            https://tinyurl.com/p5txdpaa
+          </a>
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 2,
+    name: "Housing Units Served by Private Domestic Wells in the United States",
+    img: "",
+    url: "#",
+    summary: (
+      <>
+        <p>
+          This web mapping application provides improved estimates of the
+          dependence and spatial distribution of housing units reliant on
+          private domestic wells by census block group and block. Nationally,
+          based on an understanding of well use from state well logs and the
+          housing unit density, an estimated 23 million housing units are being
+          served by this water resource, 17 per cent of the population. In 1990,
+          15.1 million households were reliant on wells. In the ensuing 30 years
+          there has been over a 50 percent increase in the number of households
+          relying on these wells. This dataset will assist in a better
+          understanding of the reliance on private domestic wells in the US,
+          better positioning local, state, and federal groups in protecting this
+          water resource from contaminant sources. Manuscript for more
+          information:{" "}
+          <a
+            className="usa-link"
+            href="https://onlinelibrary.wiley.com/doi/full/10.1111/1752-1688.12937"
+          >
+            https://onlinelibrary.wiley.com/doi/full/10.1111/1752-1688.12937
+          </a>
+        </p>
+      </>
+    ),
+    meta: (
+      <>
+        <p>
+          <strong>Source Data Description:</strong> See manuscript:{" "}
+          <a
+            className="usa-link"
+            href="https://onlinelibrary.wiley.com/doi/full/10.1111/1752-1688.12937"
+          >
+            https://onlinelibrary.wiley.com/doi/full/10.1111/1752-1688.12937
+          </a>
+        </p>
+        <p>
+          <strong>Source Data Refresh Schedule:</strong> NA
+        </p>
+        <p>
+          <strong>Intended Audience or Use Cases:</strong> General public
+        </p>
+        <p>
+          <strong>Caveats and Limitations:</strong> NA
+        </p>
+        <p>
+          <strong>Help Documentation:</strong>{" "}
+          <a
+            className="usa-link"
+            href="https://github.com/USEPA/PDW_Paper_2020"
+          >
+            https://github.com/USEPA/PDW_Paper_2020
+          </a>
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 3,
+    name: "NPDES eRule Phase 2 Implementation Dashboard",
+    img: "",
+    url: "#",
+    summary: (
+      <>
+        <p>
+          EPA is working collaboratively with states to implement Phase 2 of the
+          National Pollutant Discharge Elimination System (NPDES) Electronic
+          Reporting rule (“NPDES eRule ”). This dashboard provides information
+          on the progress in converting general permits and program reports to
+          electronic submissions (Phase 2 of the NPDES eRule) as well as EPA’s
+          coordination with states that are using EPA’s NPDES electronic
+          reporting tool (or “NeT”). Please contact Carey Johnston
+          (Johnston.Carey@epa.gov) if you would like to update information on
+          this dashboard or would like more information on EPA’s collaboration
+          and coordination activities with states.
+        </p>
+      </>
+    ),
+    meta: (
+      <>
+        <p>
+          <strong>Source Data Description:</strong> EPA updates the data for
+          this dashboard based on information provided to EPA by states.
+        </p>
+        <p>
+          <strong>Source Data Refresh Schedule:</strong> EPA updates the data
+          for this dashboard based on state information as needed. Data is
+          refreshed daily.
+        </p>
+        <p>
+          <strong>Intended Audience or Use Cases:</strong> EPA-state General
+          Permit Workgroup members , Exchange Network Electronic Data Transfer
+          Integrated Project Team , EPA and state ICIS-NPDES staff and data
+          stewards, and states planning to use NeT.
+        </p>
+        <p>
+          <strong>Caveats and Limitations:</strong> Data is being updated
+          regularly.
+        </p>
+        <p>
+          <strong>Help Documentation:</strong> Please contact Carey Johnston (
+          <a className="usa-link" href="mailto:Johnston.Carey@epa.gov">
+            Johnston.Carey@epa.gov
+          </a>
+          ) if you would like help with using this dashboard or would like more
+          information on EPA’s collaboration and coordination activities with
+          states. Documentation is available here:{" "}
+          <a
+            className="usa-link"
+            href="https://echo.epa.gov/help/npdes-erule-dashboard/general-permit-dashboard-help"
+          >
+            https://echo.epa.gov/help/npdes-erule-dashboard/general-permit-dashboard-help
+          </a>
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 4,
+    name: "WQI Portals Monitoring Coverage",
+    img: "",
+    url: "#",
+    summary: (
+      <>
+        <p>
+          This dashboard displays data from the US Water Quality Portal for the
+          last ten years to allow a user to identify who is monitoring what,
+          where and when.
+        </p>
+      </>
+    ),
+    meta: (
+      <>
+        <p>
+          <strong>Source Data Description:</strong> US. Water Quality Portals
+        </p>
+        <p>
+          <strong>Source Data Refresh Schedule:</strong> Monthly-Quarterly
+        </p>
+        <p>
+          <strong>Intended Audience or Use Cases:</strong> Water quality
+          professionals and inspection targeting.
+        </p>
+        <p>
+          <strong>Caveats and Limitations:</strong> N/A
+        </p>
+        <p>
+          <strong>Help Documentation:</strong> N/A
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 5,
+    name: "Water Quality Indicators Data Usability Improvement Project",
+    img: "",
+    url: "#",
+    summary: (
+      <>
+        <p>
+          This dashboard helps STORET and NWIS data submitters identify missing
+          metadata elements that prevent their data from being used by the Water
+          Quality Indicators (WQI) tool.
+        </p>
+      </>
+    ),
+    meta: (
+      <>
+        <p>
+          <strong>Source Data Description:</strong>{" "}
+          <a
+            className="usa-link"
+            href="https://echo.epa.gov/tools/data-downloads/wqi-data-review"
+          >
+            https://echo.epa.gov/tools/data-downloads/wqi-data-review
+          </a>
+        </p>
+        <p>
+          <strong>Source Data Refresh Schedule:</strong> Quarterly
+        </p>
+        <p>
+          <strong>Intended Audience or Use Cases:</strong> Users with data in
+          the Water Quality Portal (WQP). This dashboard allows users to
+          identify specific missing metadata elements available in the Water
+          Quality Portal data and to identify ways to improve usability of the
+          data.
+        </p>
+        <p>
+          <strong>Caveats and Limitations:</strong>{" "}
+          <a
+            className="usa-link"
+            href="https://echo.epa.gov/tools/data-downloads/wqi-data-review"
+          >
+            https://echo.epa.gov/tools/data-downloads/wqi-data-review
+          </a>
+        </p>
+        <p>
+          <strong>Help Documentation:</strong>{" "}
+          <a
+            className="usa-link"
+            href="https://echo.epa.gov/tools/data-downloads/wqi-data-review"
+          >
+            https://echo.epa.gov/tools/data-downloads/wqi-data-review
+          </a>
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 6,
+    name: "NPDES eRule Readiness and Data Completeness Dashboard",
+    img: "",
+    url: "#",
+    summary: (
+      <>
+        <p>
+          This dashboard provides EPA and states information on each state's
+          readiness to implement electronic reporting based on ICIS-NPDES data.
+          The purpose of the dashboard is to help states identify data points
+          that need attention as electronic reporting of DMRs is phased in (see
+          data sharing requirements in 40 CFR 127.26). EPA will build additional
+          charts and data visualizations over time to make it easier to evaluate
+          NPDES Electronic Reporting Rule implementation. In particular, EPA
+          will provide new charts on how well states are sharing NPDES program
+          data with EPA's ICIS-NPDES [see 40 CFR 127.23(b)]. Please contact
+          Amanda Pruzinsky and Jesse Yourish with questions, comments, or
+          suggestions
+        </p>
+      </>
+    ),
+    meta: (
+      <>
+        <p>
+          <strong>Source Data Description:</strong> ICIS-NPDES
+        </p>
+        <p>
+          <strong>Source Data Refresh Schedule:</strong> Weekly
+        </p>
+        <p>
+          <strong>Intended Audience or Use Cases:</strong> Intended for states
+          to check in on their progress in meeting eRule implementation
+          requirements
+        </p>
+        <p>
+          <strong>Caveats and Limitations:</strong> See help page
+        </p>
+        <p>
+          <strong>Help Documentation:</strong> See help page
+        </p>
+      </>
+    ),
+  },
+];
+
+export { placeholderTools };
