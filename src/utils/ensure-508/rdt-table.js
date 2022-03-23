01353e9c892505d4100c74bb665c8d79c4fbd635
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2022-03-23 11:40:55 -0600

    updated comment to be more helpful

--- src/utils/ensure-508/rdt-table.js
+++ src/utils/ensure-508/rdt-table.js
@@ -214,8 +214,8 @@ export const removeAriaSortHandlersFromDatatable = () => {
  *
  *       Inputs:
  *              data - an array of data elements that populate the table
- *              columnToReference - what the checkboxes should reference
- *              selectAllReference - what the select all checkbox shoule reference
+ *              columnToReference - what the checkboxes should reference (is an attribute of data)
+ *              selectAllReference - what the select all checkbox should reference (is just a label)
  *       Outputs:
  *              none
  *****************************************************/
