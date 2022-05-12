4aa3d40a5b6365253e5abf877aae9f2728e3a3b2
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-05-12 16:12:17 -0400

    Reverted to old file download and added target _blank to fix google analytic download issue

--- src/components/customDataDownload/DownloadFileType/DownloadFileType.js
+++ src/components/customDataDownload/DownloadFileType/DownloadFileType.js
@@ -1,48 +1,67 @@
-import React, { useState } from 'react';
-import { Button, Radio, Fieldset } from '@trussworks/react-uswds';
-import axios from 'axios';
-import download from "downloadjs";
-import { connect } from 'react-redux';
-import config from '../../../config';
+import React, { useState } from "react";
+import { Button, Radio, Fieldset } from "@trussworks/react-uswds";
+import axios from "axios";
+import { connect } from "react-redux";
+import config from "../../../config";
 
-import { constructRequestUrl } from '../../../utils/selectors/general';
-import RenderSpinner from '../../RenderSpinner/RenderSpinner';
+import { constructRequestUrl } from "../../../utils/selectors/general";
+import RenderSpinner from "../../RenderSpinner/RenderSpinner";
 import "./DownloadFileType.scss";
 
 axios.defaults.headers.common = {
-  "x-api-key": config.app.apiKey
+  "x-api-key": config.app.apiKey,
 };
 
-const DownloadFileType = ({ dataType, dataSubType, filterCriteria, totalCount, spinnerActive, setSpinnerActive}) => {
-  const [fileType, setFileType] = useState('text/csv');
+const DownloadFileType = ({
+  dataType,
+  dataSubType,
+  filterCriteria,
+  totalCount,
+  spinnerActive,
+  setSpinnerActive,
+}) => {
+  const [fileType, setFileType] = useState("text/csv");
   const [loading, setLoading] = useState(false);
 
   const onRadioChangeHandler = (event) => {
-    if (event.target.id === 'json') {
-      setFileType('application/json');
+    if (event.target.id === "json") {
+      setFileType("application/json");
     } else {
-      setFileType('text/csv');
+      setFileType("text/csv");
     }
   };
 
   const onDownloadHandler = () => {
-    setSpinnerActive(false)
+    setSpinnerActive(false);
     setLoading(true);
     axios
       .get(constructRequestUrl(dataType, dataSubType, filterCriteria, true), {
         headers: {
           Accept: fileType,
         },
-        responseType: 'blob',
+        responseType: "blob",
       })
       .then((response) => {
-        const disposition = response.headers['content-disposition'];
-        const parts = disposition !== undefined ? disposition.split('; ') : undefined;
+        const disposition = response.headers["content-disposition"];
+        const parts =
+          disposition !== undefined ? disposition.split("; ") : undefined;
 
-        if (parts !== undefined && parts[0] === 'attachment') {
-          let fileName = parts[1].replace('filename=', '');
-          fileName = fileName.replace(/"/g, '');
-          download(response.data, fileName);  
+        if (parts !== undefined && parts[0] === "attachment") {
+          //const url = window.webkitURL.createObjectURL([response.data], { type: fileType });
+
+          const url = window.URL.createObjectURL(
+            new Blob([response.data], { type: fileType })
+          );
+
+          const link = document.createElement("a");
+          let fileName = parts[1].replace("filename=", "");
+          fileName = fileName.replace(/"/g, "");
+          link.href = url;
+          link.setAttribute("download", fileName);
+          link.setAttribute("target", "_blank");
+          document.body.appendChild(link);
+          link.click();
+          link.parentNode.removeChild(link);
         }
         setLoading(false);
       })
@@ -81,12 +100,21 @@ const DownloadFileType = ({ dataType, dataSubType, filterCriteria, totalCount, s
         type="button"
         className="margin-x-1"
         onClick={() => onDownloadHandler()}
-        disabled={totalCount !== null && Number(totalCount) > Number(config.app.streamingLimit)}
+        disabled={
+          totalCount !== null &&
+          Number(totalCount) > Number(config.app.streamingLimit)
+        }
         label="JSON"
       >
         Download
       </Button>
-      {loading ? <RenderSpinner loading={loading} spinnerActive={spinnerActive} setSpinnerActive={setSpinnerActive}/> : null}
+      {loading ? (
+        <RenderSpinner
+          loading={loading}
+          spinnerActive={spinnerActive}
+          setSpinnerActive={setSpinnerActive}
+        />
+      ) : null}
     </div>
   );
 };
