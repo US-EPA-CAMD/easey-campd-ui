efbc5e0ab3567ec24aa67cabbf967a7c75cf3249
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-08-15 16:08:24 -0400

    Reverted to process.env for public url

--- src/components/SubHeader/SubHeader.js
+++ src/components/SubHeader/SubHeader.js
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Header, Title, Button } from '@trussworks/react-uswds';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { Header, Title, Button } from "@trussworks/react-uswds";
 
-import SubHeaderInfo from '../SubHeaderInfo/SubHeaderInfo';
-import SubHeaderNav from '../SubHeaderNav/SubHeaderNav';
+import SubHeaderInfo from "../SubHeaderInfo/SubHeaderInfo";
+import SubHeaderNav from "../SubHeaderNav/SubHeaderNav";
 import {
   subHeaderMenuList,
   subHeaderUtilityList,
-} from '../../utils/constants/menuTopics';
-import useCheckWidth from '../../utils/hooks/useCheckWidth';
+} from "../../utils/constants/menuTopics";
+import useCheckWidth from "../../utils/hooks/useCheckWidth";
 import SubHeaderNavMobile from "../SubHeaderNavMobile/SubHeaderNavMobile";
 
-import './SubHeader.scss';
+import "./SubHeader.scss";
 
 const SubHeader = () => {
   const pathname = useLocation().pathname;
-  const cddPath = ['/data/custom-data-download', '/manage-data-download','/data', '/data/bulk-data-files'];
+  const cddPath = [
+    "/data/custom-data-download",
+    "/manage-data-download",
+    "/data",
+    "/data/bulk-data-files",
+  ];
   const initialCategorySelected = [
-    pathname === '/',
+    pathname === "/",
     cddPath.includes(pathname),
     false,
     false,
@@ -32,15 +37,15 @@ const SubHeader = () => {
   const [navDropdownOpen, setNavDropdownOpen] = useState(initialNavOpen);
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [mobileMenuFocus, setMobileMenuFocus] = useState({
-    collapseButton: document.querySelector('#collapseButton'),
-    expandButton: document.querySelector('#expandButton'),
+    collapseButton: document.querySelector("#collapseButton"),
+    expandButton: document.querySelector("#expandButton"),
   });
   const { collapseButton, expandButton } = mobileMenuFocus;
-  const isDesktop = useCheckWidth([1024, 1200])
+  const isDesktop = useCheckWidth([1024, 1200]);
   useEffect(() => {
     setMobileMenuFocus({
-      collapseButton: document.querySelector('#collapseButton'),
-      expandButton: document.querySelector('#expandButton')
+      collapseButton: document.querySelector("#collapseButton"),
+      expandButton: document.querySelector("#expandButton"),
     });
   }, [showMobileMenu]);
   const handleToggleNavDropdown = (column, isUtility) => {
@@ -56,33 +61,37 @@ const SubHeader = () => {
   };
 
   const handleCloseNavDropdown = (isUtility) => {
-    setNavDropdownOpen(initialNavOpen)
-  }
+    setNavDropdownOpen(initialNavOpen);
+  };
   return (
     <div className="subheader-wrapper">
       <Header
         className="padding-y-2 mobile-lg:padding-x-2 tablet:padding-x-4 widescreen:padding-x-10"
         style={{
           backgroundImage:
-            pathname === '/'
-              ? `url(${window._env_.PUBLIC_URL + '/images/header-bg.png'})`
+            pathname === "/"
+              ? `url(${process.env.PUBLIC_URL + "/images/header-bg.png"})`
               : null,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center top',
-          alignSelf: 'top center',
+          backgroundSize: "cover",
+          backgroundPosition: "center top",
+          alignSelf: "top center",
         }}
       >
         <div className="usa-nav-container custom-wrapper clearfix padding-x-0">
           <Title className="logo-wrapper float-left margin-0 desktop-lg:padding-top-2">
             <span className="display-block display-flex desktop:display-block desktop-lg:display-flex">
               <img
-                src={`${window._env_.PUBLIC_URL}/images/campd-mark.svg`}
+                src={`${process.env.PUBLIC_URL}/images/campd-mark.svg`}
                 alt="CAMPD Logo"
               />
               <h1 className="display-inline-block text-white text-heavy font-sans-3xl margin-0">
                 CAMPD
               </h1>
-              <span className={`display-none ${isDesktop ? 'width-card':'width-full'} tablet:display-inline-block desktop:display-block margin-left-8 tablet:margin-left-1 tablet:margin-top-1 desktop:margin-left-8 desktop:margin-top-0 desktop-lg:margin-left-1 desktop-lg:margin-top-1 desktop-lg:display-inline-block text-white text-normal font-sans-md text-wrap`}>
+              <span
+                className={`display-none ${
+                  isDesktop ? "width-card" : "width-full"
+                } tablet:display-inline-block desktop:display-block margin-left-8 tablet:margin-left-1 tablet:margin-top-1 desktop:margin-left-8 desktop:margin-top-0 desktop-lg:margin-left-1 desktop-lg:margin-top-1 desktop-lg:display-inline-block text-white text-normal font-sans-md text-wrap`}
+              >
                 Clean Air Markets Program Data
               </span>
             </span>
@@ -94,10 +103,10 @@ const SubHeader = () => {
             <Button
               className={
                 showMobileMenu
-                  ? 'display:none opacity-0'
-                  : 'float-right bg-transparent margin-0 position-relative top-1 padding-right-3'
+                  ? "display:none opacity-0"
+                  : "float-right bg-transparent margin-0 position-relative top-1 padding-right-3"
               }
-              id={'expandButton'}
+              id={"expandButton"}
               tabIndex={showMobileMenu ? -1 : 0}
               aria-expanded={showMobileMenu}
               onClick={() => {
@@ -106,18 +115,18 @@ const SubHeader = () => {
               }}
             >
               <img
-                src={`${window._env_.PUBLIC_URL}/images/icons/mobile-menu-expand.svg`}
+                src={`${process.env.PUBLIC_URL}/images/icons/mobile-menu-expand.svg`}
                 alt="Expandable Menu"
-                className={'position-absolute bottom-1px'}
+                className={"position-absolute bottom-1px"}
               />
             </Button>
             <Button
               className={
                 showMobileMenu
-                  ? 'float-right bg-transparent margin-0 position-relative top-1 padding-right-3'
-                  : 'display:none opacity-0'
+                  ? "float-right bg-transparent margin-0 position-relative top-1 padding-right-3"
+                  : "display:none opacity-0"
               }
-              id={'collapseButton'}
+              id={"collapseButton"}
               tabIndex={showMobileMenu ? 0 : -1}
               aria-expanded={showMobileMenu}
               onClick={() => {
@@ -126,12 +135,12 @@ const SubHeader = () => {
               }}
             >
               <img
-                src={`${window._env_.PUBLIC_URL}/images/icons/mobile-menu-collapse.svg`}
+                src={`${process.env.PUBLIC_URL}/images/icons/mobile-menu-collapse.svg`}
                 alt="Collapsable Menu"
                 className={
                   showMobileMenu
-                    ? 'position-absolute bottom-1px'
-                    : 'display-none'
+                    ? "position-absolute bottom-1px"
+                    : "display-none"
                 }
               />
             </Button>
@@ -142,7 +151,10 @@ const SubHeader = () => {
               pathname={pathname}
             />
           </div>
-          <div className="display-flex flex-column flex-align-end" id="sub-header-nav">
+          <div
+            className="display-flex flex-column flex-align-end"
+            id="sub-header-nav"
+          >
             <SubHeaderNav
               pathname={pathname}
               menuList={subHeaderUtilityList}
@@ -162,7 +174,7 @@ const SubHeader = () => {
             />
           </div>
         </div>
-        {pathname === '/' && <SubHeaderInfo />}
+        {pathname === "/" && <SubHeaderInfo />}
       </Header>
     </div>
   );
