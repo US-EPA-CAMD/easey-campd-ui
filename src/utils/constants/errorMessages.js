12a442aeef1646ee212a0a823afea7c3e7468112
AbuFofanah <abufofanah@cvpcorp.com>
2022-08-29 17:05:28 -0400

    added missing files

---
+++ src/utils/constants/errorMessages.js
@@ -0,0 +1,16 @@
+export const errorMessages = {
+  contentManager:
+    "All of the content on this page may not be available. If you continue to encounter this issue, contact CAMPD support: campd-support@camdsupport.com",
+  filterLogic:
+    "We're currently experiencing technical issues. If you continue to encounter this issue, contact CAMPD support: campd-support@camdsupport.com",
+  MDMRetrieval:
+    "We're currently experiencing technical issues. If you continue to encounter this issue, contact CAMPD support: campd-support@camdsupport.com",
+  dataPreview:
+    "We're currently experiencing technical issues. If you continue to encounter this issue, contact CAMPD support: campd-support@camdsupport.com",
+  download:
+    "We're currently experiencing technical issues. If you continue to encounter this issue, contact CAMPD support: campd-support@camdsupport.com",
+  bulkDataFiles:
+    "We're currently experiencing technical issues. If you continue to encounter this issue, contact CAMPD support: campd-support@camdsupport.com",
+  contactUs:
+    "All of the content on this page may not be available. If you continue to encounter this issue, contact CAMPD support: campd-support@camdsupport.com",
+};
