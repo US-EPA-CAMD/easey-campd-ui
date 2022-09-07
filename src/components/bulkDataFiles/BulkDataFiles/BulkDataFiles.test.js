ee833bef64431b9d6e0235287079512b308068ff
AbuFofanah <abufofanah@cvpcorp.com>
2022-09-07 11:57:14 -0400

    changing data type clears selected files

--- src/components/bulkDataFiles/BulkDataFiles/BulkDataFiles.test.js
+++ src/components/bulkDataFiles/BulkDataFiles/BulkDataFiles.test.js
@@ -17,6 +17,7 @@ jest.mock('react-router-dom', () => ({
     push: jest.fn(),
   }),
 }));
+jest.setTimeout(30000);
 
 /*****
  */
@@ -39,8 +40,8 @@ describe('Manage Bulk Data Files component: ',  () => {
     expect(downloadButton).toBeDisabled();
   });
 
-  test('download button is enabled after files are selected', () => {
-    const { getByRole } = render(
+  test('download button is enabled after files are selected', async() => {
+    const { getByRole, findAllByRole } = render(
       <Provider store={store}>
         <MemoryRouter>
           <BulkDataFiles
@@ -51,18 +52,16 @@ describe('Manage Bulk Data Files component: ',  () => {
         </MemoryRouter>
       </Provider>
     );
-    const checkbox = getByRole('checkbox', {
-      name: /select-row-4/i
-    })
-    fireEvent.click(checkbox);
+    const checkbox = await findAllByRole('checkbox')
+    fireEvent.click(checkbox[1])
     const downloadButton = getByRole('button', {
       name: /download/i
     });
     expect(downloadButton).not.toBeDisabled();
   });
 
-  test('number of files is updated when files are added or removed', () => {
-    const { getByRole, getByText } = render(
+  test('number of files is updated when files are added or removed', async() => {
+    const { getByText, findAllByRole } = render(
       <Provider store={store}>
         <MemoryRouter>
           <BulkDataFiles
@@ -73,12 +72,10 @@ describe('Manage Bulk Data Files component: ',  () => {
         </MemoryRouter>
       </Provider>
     );
-    const checkbox1 = getByRole('checkbox', {
-      name: /select-row-4/i
-    });
-    const checkbox2 = getByRole('checkbox', {
-      name: /select-row-5/i
-    })
+    const checkboxes = await findAllByRole('checkbox')
+
+    const checkbox1 = checkboxes[1]
+    const checkbox2 = checkboxes[2]
     fireEvent.click(checkbox1);
     const fileCount= getByText(/files selected: 1/i)
     expect(fileCount).toBeInTheDocument();
@@ -112,7 +109,7 @@ test('sections render without errors', async () => {
 
 
 test('file size is updated when files are added or removed', async () => {
-  const { findByRole, getByText } = render(
+  const { findAllByRole, getByText } = render(
     <Provider store={store}>
       <MemoryRouter>
         <BulkDataFiles
@@ -123,17 +120,15 @@ test('file size is updated when files are added or removed', async () => {
       </MemoryRouter>
     </Provider>
   );
-  const checkbox = await findByRole('checkbox', {
-    name: /select-row-4/i
-  })
-  fireEvent.click(checkbox);
-  fireEvent.click(checkbox);
+  const checkbox = await findAllByRole('checkbox')
+  fireEvent.click(checkbox[1])
+  fireEvent.click(checkbox[1])
   const updatedFileSize= getByText(/size:/i)
   expect(updatedFileSize).toBeInTheDocument();
 });
 
 test('file size is reset when filters are cleared', async () => {
-  const { findByRole, getByText, getAllByText } = render(
+  const { findAllByRole, getByText, getAllByText } = render(
     <Provider store={store}>
       <MemoryRouter>
         <BulkDataFiles
@@ -144,18 +139,17 @@ test('file size is reset when filters are cleared', async () => {
       </MemoryRouter>
     </Provider>
   );
-  const checkbox = await findByRole('checkbox', {
-    name: /select-row-4/i
-  })
-  fireEvent.click(checkbox)
+  const checkbox = await findAllByRole('checkbox')
+  fireEvent.click(checkbox[1])
   const clearAllButton = getAllByText(/clear all/i)[0];
   fireEvent.click(clearAllButton);
   const updatedFileSize= getByText(/size:/i)
   expect(updatedFileSize).toBeInTheDocument()
 });
 
-test('download button is disabled if file size exceeds download limit', async () => {
-  const {findByRole} = render(
+test('file size is reset when selected data type is changed', async () => {
+  window.confirm = jest.fn(() => true)
+  const { getByText, findByTestId, findAllByRole } = render(
     <Provider store={store}>
       <MemoryRouter>
         <BulkDataFiles
@@ -166,9 +160,54 @@ test('download button is disabled if file size exceeds download limit', async ()
       </MemoryRouter>
     </Provider>
   );
-  const allFiles = await findByRole('checkbox', {
-    name: /select-all-rows/i
-  })
+  const dataTypeFilter = await findByTestId('dataType-select');
+  fireEvent.change(dataTypeFilter, { target: { value: "Emissions" } });
+  const checkbox = await findAllByRole('checkbox')
+  fireEvent.click(checkbox[0])
+  fireEvent.change(dataTypeFilter, { target: { value: "EDR" } });
+  screen.debug()
+  const updatedFileSize= getByText(/size:/i)
+  expect(updatedFileSize).toBeInTheDocument()
+});
+
+test('file size is not reset when year filter is changed', async () => {
+  window.confirm = jest.fn(() => true)
+  const { getByText, findByTestId, findAllByRole } = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <BulkDataFiles
+          loadBulkDataFilesDispatcher= {jest.fn()}
+          updateBulkDataFilesDispacher={jest.fn()}
+          dataTable={dataTable}
+        />
+      </MemoryRouter>
+    </Provider>
+  );
+  const dataTypeFilter = await findByTestId('dataType-select');
+  fireEvent.change(dataTypeFilter, { target: { value: "Emissions" } });
+  const checkbox = await findAllByRole('checkbox')
+  fireEvent.click(checkbox[0])
+  const subTypeFilter = await findByTestId('subtype-select');
+  fireEvent.change(subTypeFilter, { target: { value: "Hourly" } });
+  screen.debug()
+  const updatedFileSize= getByText(/size: 4.04 gb/i)
+  expect(updatedFileSize).toBeInTheDocument()
+});
+
+test('download button is disabled if file size exceeds download limit', async () => {
+  const {findByRole, findAllByRole} = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <BulkDataFiles
+          loadBulkDataFilesDispatcher= {jest.fn()}
+          updateBulkDataFilesDispacher={jest.fn()}
+          dataTable={dataTable}
+        />
+      </MemoryRouter>
+    </Provider>
+  );
+  const checkbox = await findAllByRole('checkbox')
+  const allFiles = checkbox[0]
   fireEvent.click(allFiles);
   expect(findByRole('button', {
     name: /download/i
