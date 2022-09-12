633dfae31dfc2d1a441d3a7b8e442927ffe52de6
AbuFofanah <abufofanah@cvpcorp.com>
2022-09-12 08:02:46 -0400

    add api error alert component

---
+++ src/components/ApiErrorAlert/ApiErrorAlert.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import ApiErrorAlert from './ApiErrorAlert';
+import configureStore from '../../store/configureStore.dev';
+import initialState from '../../store/reducers/initialState';
+import { Provider } from 'react-redux';
+
+let store = configureStore(initialState);
+
+describe('ApiErrorAlert', () => {
+    test("does not render alert if there is no error", async () => {
+        const { container } = render(
+          <Provider store={store}>
+            <MemoryRouter>
+                <ApiErrorAlert setApiErrorDispatcher={jest.fn()} apiErrors={initialState.apiErrors}/>
+            </MemoryRouter>
+          </Provider>
+        );
+        const alert = container.querySelector('#api-error-banner');
+        expect(alert).toBeFalsy();
+      });
+});
