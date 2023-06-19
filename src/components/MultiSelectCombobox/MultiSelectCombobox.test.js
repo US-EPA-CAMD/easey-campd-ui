29a86de1345bc733a44efddc7aa3d93f0bf2e2c6
abufofanah <cvp@Abus-MBP.lan>
2023-06-19 09:05:53 -0400

    build error fix

--- src/components/MultiSelectCombobox/MultiSelectCombobox.test.js
+++ src/components/MultiSelectCombobox/MultiSelectCombobox.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   cleanup,
-  fireEvent,
   within,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -293,6 +292,7 @@ const facilities = [
 ];
 const items = facilities.map(f=> ({id: f.facilityId, label:`${f.facilityName} (${f.facilityId})`, selected:false, enabled:true}));
 
+
 describe('MultiSelectCombobox Component', () => {
   let query;
   beforeEach(() => {
@@ -333,23 +333,25 @@ describe('MultiSelectCombobox Component', () => {
     expect(searchbox).toBeInTheDocument();
     await userEvent.click(searchbox);
     const options = getAllByTestId("multi-select-option");
-    fireEvent.click(options[0]);
-    fireEvent.click(options[1]);
+    await userEvent.click(options[0]);
+    await userEvent.click(options[1]);
     expect(getAllByTestId('button').length).toBe(2);
   });
 
-  test('It should search using input box for facilities in listboxt', () => {
-    const { getByTestId, getAllByTestId, getByRole} = query;
-    const searchbox = getByTestId("input-search");
-    searchbox.click();
-    fireEvent.change(searchbox, { target: { value: 'Barry' } })
+  test('It should search using input box for facilities in listboxt', async() => {
+    const { getByTestId, getAllByTestId, getByRole, findByTestId} = query;
+    const searchbox = await findByTestId("input-search");
+    await userEvent.click(searchbox)
+    await userEvent.type(searchbox, 'Barry')
     expect(searchbox.value).toBe('Barry');
     expect(within(getByTestId("multi-select-listbox")).getAllByTestId('multi-select-option').length).toBe(1);
-    fireEvent.keyDown(searchbox, {key: 'Tab', code: 9});
-    fireEvent.keyDown(searchbox, {key: 'Enter', code: 'Enter'})
+    // await userEvent.keyDown(searchbox, {key: 'Tab', code: 9});
+    await userEvent.tab()
+    await userEvent.type(searchbox, '{enter}');
+    // await userEvent.keyDown(searchbox, {key: 'Enter', code: 'Enter'})
     expect(searchbox.value).toBe('Barry');
     expect(getAllByTestId("multi-select-option").length).toBe(1);
-    fireEvent.click(getByTestId("multi-select-option"));
+    await userEvent.click(getByTestId("multi-select-option"));
     expect(getByRole("button", {name: "Barry (3)"})).toBeDefined();
     //expect(getAllByTestId("multi-select-option").length).toBe(facilities.length);
   })
