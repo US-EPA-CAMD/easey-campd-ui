3b9cbc3ecccaddb8acbfcb25a762c67c72126c55
Jon Maxwell Diebold <max.diebold@erg.com>
2025-01-28 13:49:04 -0500

    fix(6555): update CSP to allow GA

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import TagManager from "react-gtm-module";
+import TagManager from "@sooro-io/react-gtm-module";
 
 import config from "./config";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
 import configureStore from "./store/configureStore.dev";
 
-import '@trussworks/react-uswds/lib/index.css';
-import './styles/index.scss';
+import "@trussworks/react-uswds/lib/index.css";
+import "./styles/index.scss";
 
 const store = configureStore();
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 if (config.app.googleAnalyticsEnabled) {
-  let tagManagerArgs = { gtmId: config.app.googleAnalyticsContainerId };
+  // @ts-ignore
+  const nonce = window.__CSP_NONCE__;
+  const tagManagerArgs = {
+    gtmId: config.app.googleAnalyticsContainerId,
+    ...(nonce ? { nonce } : {}),
+  };
   TagManager.initialize(tagManagerArgs);
 }
 
